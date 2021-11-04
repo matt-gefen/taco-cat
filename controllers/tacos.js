ac7e9e586c6e73f3ba89c2139d7ba6cb16e32d7c
mattgefen <matt.gefen@gmail.com>
2021-11-04 08:44:50 -0600

    creating tacos

--- controllers/tacos.js
+++ controllers/tacos.js
@@ -1,9 +1,34 @@
 import {Taco} from '../models/taco.js'
 
 function index(req, res) {
-  res.send('TACOTACOTACO')
+  // find all tacos
+  Taco.find({})
+    .then(tacos => {
+      // do something with the tacos
+      res.render("tacos/index", {
+        title: "🌮",
+        tacos
+      })
+    })
+    .catch(
+      error => {
+        console.log(error)
+        res.redirect("/tacos")
+  })
+}
+
+function create(req, res) {
+  req.body.owner = req.user.profile._id
+	req.body.tasty = !!req.body.tasty
+  Taco.create(req.body)
+    .then(res.redirect('/tacos'))
+    .catch(error => {
+      console.log(error)
+      res.redirect("/tacos")
+    }) 
 }
 
 export {
-  index
+  index,
+  create
 }
--- routes/tacos.js
+++ routes/tacos.js
@@ -1,9 +1,11 @@
 import {Router} from 'express'
-import tacosCtrl from '../controllers/tacos.js'
+import * as tacosCtrl from '../controllers/tacos.js'
+import { isLoggedIn } from '../middleware/middleware.js'
 
 const router = Router()
 
 router.get('/', tacosCtrl.index)
+router.post('/', isLoggedIn, tacosCtrl.create)
 
 export {
   router
