fd08d6e529a1199571a1c30bd361eef03b2f392a
mattgefen <matt.gefen@gmail.com>
2021-11-04 07:16:46 -0600

    Add taco model

---
+++ models/taco.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose"
+
+const Schema = mongoose.Schema
+
+const tacoSchema = new Schema (
+  {
+    name: String,
+    tasty: Boolean,
+    owner: {type: Schema.Types.ObjectId, ref: 'Profile'}
+  }
+)
+
+const Taco = mongoose.model("Taco", tacoSchema)
+
+export {
+  Taco
+}
