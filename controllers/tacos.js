4d7ee411a23c8c515a99ba2b69577a68c37c847e
mattgefen <matt.gefen@gmail.com>
2021-11-04 09:35:40 -0600

    flippin' tacos

--- controllers/tacos.js
+++ controllers/tacos.js
@@ -28,7 +28,42 @@ function create(req, res) {
     }) 
 }
 
+function show(req,res) {
+  Taco.findById(req.params.id)
+  .populate("owner")
+    .then(taco =>
+      {
+        res.render("tacos/show", {
+          taco,
+          title: `🌮 ${taco.name}`
+        })
+      }
+    )
+    .catch(error => {
+      console.log(error)
+      res.redirect("/tacos")
+    })
+}
+
+function flipTasty(req, res) {
+  console.log(req.params)
+  Taco.findById(req.params.id) 
+    .then(taco => {
+      taco.tasty = !taco.tasty
+      taco.save()
+    })
+    .then(()=> {
+      res.redirect(`/tacos/${taco._id}`)
+    })
+    .catch(error => {
+      console.log(error)
+      res.redirect(`/tacos/${req.params.id}`)
+    })
+}
+
 export {
   index,
-  create
+  create,
+  show,
+  flipTasty
 }
--- routes/tacos.js
+++ routes/tacos.js
@@ -6,6 +6,8 @@ const router = Router()
 
 router.get('/', tacosCtrl.index)
 router.post('/', isLoggedIn, tacosCtrl.create)
+router.get('/:id', tacosCtrl.show)
+router.patch('/:id/flip-tasty', isLoggedIn, tacosCtrl.flipTasty)
 
 export {
   router
